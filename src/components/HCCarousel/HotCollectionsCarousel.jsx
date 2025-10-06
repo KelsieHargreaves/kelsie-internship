94ff52c8fddbd0b840bb4f337b2deeea6d38a42f
KelsieHargreaves <kelsie.hargreaves@yahoo.com>
2025-10-06 18:49:36 -0500

    fix merge issues

--- src/components/HCCarousel/HotCollectionsCarousel.jsx
+++ src/components/HCCarousel/HotCollectionsCarousel.jsx
@@ -89,7 +89,6 @@ const HotCollectionsCarousel = () => {
                 </div>
                 <div className="nft_coll_pp">
                   <Link to={`/author/${item.authorId}`}>
-                  <Link to={`/author/${item.authorId}`}>
                     <img
                       className="lazy pp-coll"
                       src={item.authorImage}
