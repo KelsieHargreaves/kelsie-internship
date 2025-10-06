de91993035f927cc5cffe2df04408e235d8d187f
KelsieHargreaves <kelsie.hargreaves@yahoo.com>
2025-10-05 21:35:31 -0500

    explore page completed

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -7,7 +7,8 @@ import Skeleton from "../UI/Skeleton";
 import "keen-slider/keen-slider.min.css";
 import KeenSlider from "keen-slider";
 import { useKeenSlider } from "keen-slider/react";
-import { use } from "react";
+import ItemCard from "../ItemCard";
+
 
 const NewItems = () => {
   const [items, setItems] = useState([]);
@@ -112,60 +113,7 @@ const NewItems = () => {
             </div>
           </div>
           <div ref={sliderRef} className="keen-slider">
-            {items.map((data, index) => (
-              <div className="keen-slider__slide item" key={index}>
-                <div className="nft__item">
-                  <div className="author_list_pp">
-                    <Link
-                      to="/author"
-                      data-bs-toggle="tooltip"
-                      data-bs-placement="top"
-                      title="Creator: Monica Lucas"
-                    >
-                      <img className="lazy" src={data.authorImage} alt="" />
-                      <i className="fa fa-check"></i>
-                    </Link>
-                  </div>
-                  <div className="de_countdown">{countdowns[index]}</div>
-                  <div className="nft__item_wrap">
-                    <div className="nft__item_extra">
-                      <div className="nft__item_buttons">
-                        <button>Buy Now</button>
-                        <div className="nft__item_share">
-                          <h4>Share</h4>
-                          <a href="" target="_blank" rel="noreferrer">
-                            <i className="fa fa-facebook fa-lg"></i>
-                          </a>
-                          <a href="" target="_blank" rel="noreferrer">
-                            <i className="fa fa-twitter fa-lg"></i>
-                          </a>
-                          <a href="">
-                            <i className="fa fa-envelope fa-lg"></i>
-                          </a>
-                        </div>
-                      </div>
-                    </div>
-                    <Link to="/item-details">
-                      <img
-                        src={data.nftImage}
-                        className="lazy nft__item_preview"
-                        alt=""
-                      />
-                    </Link>
-                  </div>
-                  <div className="nft__item_info">
-                    <Link to="/item-details">
-                      <h4>{data.title}</h4>
-                    </Link>
-                    <div className="nft__item_price">{data.price} ETH</div>
-                    <div className="nft__item_like">
-                      <i className="fa fa-heart"></i>
-                      <span>{data.likes}</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            ))}
+             <ItemCard items={items} countdowns={countdowns} isSlider={true} />       
             <button
               className="slider-button left"
               onClick={() => instanceRef.current?.prev()}
